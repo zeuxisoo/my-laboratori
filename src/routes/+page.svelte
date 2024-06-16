f5ceca74d8c69578b42a8408a11f8060247c2927
Zeuxis Lo <seekstudio@gmail.com>
2024-06-16 21:37:02 +0800

    update global and home layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,16 @@
-Home page
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div class="card glass">
+        <figure class="bg-slate-100">
+            <img src="https://placehold.co/450x285" alt="card" />
+        </figure>
+        <div class="card-body p-3">
+            <div class="card-title">Title</div>
+            <div class="card-description">
+                this is description
+            </div>
+            <div class="card-actions justify-end">
+                <button class="btn btn-sm">More</button>
+            </div>
+        </div>
+    </div>
+</div>
