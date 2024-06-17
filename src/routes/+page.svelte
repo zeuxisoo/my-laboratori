8a35c75055d3d3965e3ab9cf73cffe35b5d1f3e2
Zeuxis Lo <seekstudio@gmail.com>
2024-06-17 20:56:24 +0800

    show loading status when fetching projects json

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,21 +5,25 @@ export let data;
 const projects = data.projects;
 </script>
 
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-    {#each projects as project}
-        <div class="card glass">
-            <figure class="bg-slate-100 min-h-72">
-                <img src={project.thumb} alt={project.caption} />
-            </figure>
-            <div class="card-body p-3">
-                <div class="card-title">{project.caption}</div>
-                <div class="card-description">
-                    {project.description}
-                </div>
-                <div class="card-actions justify-end">
-                    <a href="/project/{project.id}" class="btn btn-sm">More</a>
+{#await projects}
+    <div>Loading...</div>
+{:then projects}
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {#each projects.reverse() as project}
+            <div class="card glass">
+                <figure class="bg-slate-100 min-h-72">
+                    <img src={project.thumb} alt={project.caption} />
+                </figure>
+                <div class="card-body p-3">
+                    <div class="card-title">{project.caption}</div>
+                    <div class="card-description">
+                        {project.description}
+                    </div>
+                    <div class="card-actions justify-end">
+                        <a href="/project/{project.id}" class="btn btn-sm">More</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    {/each}
-</div>
+        {/each}
+    </div>
+{/await}
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,8 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch  }) {
     const response = await fetch('/data/projects.json');
-    const projects = await response.json();
 
     return {
-        projects: projects.reverse()
+        projects: response.json()
     }
 }
